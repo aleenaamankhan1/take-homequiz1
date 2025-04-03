0f98b7cb3b614f896a648ddcc30788c56f2cf57a
Administrator <aleenaak@uw.edu>
2025-04-03 14:58:59 -0700

    .

--- main.cpp
+++ main.cpp
@@ -1,16 +1,58 @@
 #include <iostream>
 
-// TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-int main() {
-    // TIP Press <shortcut actionId="RenameElement"/> when your caret is at the
-    // <b>lang</b> variable name to see how CLion can help you rename it.
-    auto lang = "C++";
-    std::cout << "Hello, World " << lang << "!\n";
-    return 0;
+void test();
+
+class Fraction {
+public:
+    int numerator;
+    int denominator;
+
+    Fraction(int numerator, int denominator) {
+        this->numerator = numerator;
+        this->denominator = denominator;
+    }
+
+    std::string to_string() {
+        return std::to_string(numerator) + "/" + std::to_string(denominator);
+    }
+
+    bool operator==(Fraction &other) {
+        return this->numerator == other.numerator &&
+               this->denominator == other.denominator;
+    }
+};
+
+Fraction reduce(Fraction input) {
+    return Fraction(-1, -1); // FIXED: Changed `{}` to `()`
 }
 
-// TIP See CLion help at <a
-// href="https://www.jetbrains.com/help/clion/">jetbrains.com/help/clion/</a>.
-//  Also, you can try interactive lessons for CLion by selecting
-//  'Help | Learn IDE Features' from the main menu.
+int main() {
+    test();
+}
+
+void test() {
+    // Reduce given fraction to minimum
+    // 2/4 -> 1/2
+    Fraction inputs[4] = {Fraction(1, 2),
+                          Fraction(2, 4),  // FIXED: Changed `{}` to `()`
+                          Fraction(4, 2),
+                          Fraction(6, 8)};
+
+    Fraction outputs[4] = {Fraction(1, 2),
+                           Fraction(1, 2),  // FIXED: Changed `{}` to `()`
+                           Fraction(2, 1),
+                           Fraction(3, 4)};
+
+    for (int i = 0; i < 4; ++i) {
+        Fraction input = inputs[i];
+        Fraction expect = outputs[i];
+
+        Fraction actual = reduce(input);
+
+        if (expect == actual) {
+            std::cout << "PASS!" << std::endl;
+        } else {
+            std::cout << "FAIL! case is: " << input.to_string() << std::endl;
+        }
+    }
+}
