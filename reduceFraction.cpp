71abae0ba804c91bb21cdb524a55d52441bdc297
Administrator <aleenaak@uw.edu>
2025-04-08 14:29:27 -0700

    till 20 number

--- reduceFraction.cpp
+++ reduceFraction.cpp
@@ -1,6 +1,5 @@
 #include <iostream>
-
-void test();
+#include <string>
 
 class Fraction {
 public:
@@ -22,59 +21,55 @@ public:
     }
 };
 
-Fraction reduce(Fraction input) {
+void reduce(Fraction *input) {
     int divider = 2;
-    while (divider <= input.numerator && divider <= input.denominator) {
-        if (input.numerator % divider == 0 &&
-            input.denominator % divider == 0) {
-            input.numerator /= divider;
-            input.denominator /= divider;
+    while (divider <= input->numerator && divider <= input->denominator) {
+        if (input->numerator % divider == 0 &&
+            input->denominator % divider == 0) {
+            input->numerator /= divider;
+            input->denominator /= divider;
             continue;
-            }
+        }
         divider++;
     }
-    return input;
 }
 
 int main() {
     int var = 19;
-
-
     int* p_var = &var;
 
     double num = 1.55;
-
-    int c = 'c';
-
     double* p_num = &num;
 
-    (*p_num)++;
+    (*p_num)++;  // Increment the double value
 
-    int data[] = {1, 2, 3, 4, 5};
-
-    int *array = new int[20];
+    // Corrected initialization with zeroed values for the dynamic array
+    int* array = new int[20]();
     for (int i = 0; i < 20; i++) {
-        array[i] = i+2;
+        array[i] = i + 1;
     }
 
-    for (int i = 0; i < 5; ++i) {
-        std::cout<< *(array+i) <<std::endl;
+    for (int i = 0; i < 20; i++) {
+        std::cout << array[i] << std::endl;
     }
 
 
-
+    int* xyz = array + 3;  // Pointer to element at index 3 of the array
+    float sum = 0;
+    for (int i = 0; i < 17; ++i) {  // Corrected: Loop within the valid range
+        sum += xyz[i];
+    }
+    std::cout << "average is " << sum / 17 << std::endl;
 }
 
 void test() {
     // Reduce given fraction to minimum
-    // 2/4 -> 1/2
     Fraction inputs[4] = {Fraction(1, 2),
-                          Fraction(2, 4),  // FIXED: Changed `{}` to `()`
+                          Fraction(2, 4),
                           Fraction(4, 2),
                           Fraction(6, 8)};
-
     Fraction outputs[4] = {Fraction(1, 2),
-                           Fraction(1, 2),  // FIXED: Changed `{}` to `()`
+                           Fraction(1, 2),
                            Fraction(2, 1),
                            Fraction(3, 4)};
 
@@ -82,9 +77,9 @@ void test() {
         Fraction input = inputs[i];
         Fraction expect = outputs[i];
 
-        Fraction actual = reduce(input);
+        reduce(&input);
 
-        if (expect == actual) {
+        if (expect == input) {
             std::cout << "PASS!" << std::endl;
         } else {
             std::cout << "FAIL! case is: " << input.to_string() << std::endl;
